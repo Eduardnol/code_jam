5df7ec5955586a2da48d702bef345e41d72aa8de
Eduardo <eduardo.nolla@outlook.com>
2021-03-27 10:19:27 +0100

    Initial Comit

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Eduardo Nolla
+ * @version 1.0
+ * @since 27/03/2021
+ */
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
